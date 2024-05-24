8693a9657fdab415d7e6136ddd2f0fbd6cda0cd5
Bastian B <bastian.berrios@sansano.usm.cl>
2024-05-24 14:02:15 -0400

    Initial commit

---
+++ login/core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+# Create your views here.
+
+def home (request):
+    return  render (request, 'core/home.html')
+
+@login_required
+def listado(request):
+    return render(request, 'core/listado.html')
+
+def exit(request):
+    logout(request)
+    return redirect('home')
